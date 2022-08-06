957c1e81c3924d280143be10456912d35f5f720c
zt07 <zaheeb07@gmail.com>
2022-08-05 21:16:12 -0400

    TODO: Add loops

--- Brainfuck.c
+++ Brainfuck.c
@@ -3,9 +3,10 @@
 #include <string.h>
 
 int eval(char *code){
-    int ptr = 0;
-    char tape[30000] = {0};
-    for (int i = 0; i<strlen(code); i++){
+    //Goes thorugh array manipulating digits. As dsetermined through the program.
+    int ptr = 0;                   
+    char tape[30000] = {0}; //Initialize a Array or tape with 30,000 elements with the value of 0 in each index.
+    for (int i = 0; i<strlen(code); i++){ // Goes through the code arguemnt passedm.
         switch (code[i]){
             case '>':
 				ptr++;
@@ -15,8 +16,13 @@ int eval(char *code){
 				break;
             case '+':
                 tape[ptr]++;
+                break;
             case '-':
                 tape[ptr]--;
+                break;
+            case '.':
+            putchar(tape[ptr]);
+            break;
             
 
         
@@ -27,5 +33,6 @@ int eval(char *code){
 }
 
 int main (void){
-    
+    printf("\n");
+    eval(">+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.");
 }
